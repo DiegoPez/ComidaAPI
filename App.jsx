246560bf0c947c5936b29ef6366ddaf3671b717d
patryck17HUB <ppoumian17@alumnos.uaq.mx>
2024-02-27 14:13:22 -0600

    BORRAR

--- App.jsx
+++ App.jsx
@@ -21,20 +21,7 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container : {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  button : {
-    backgroundColor: 'blue',
-    padding: 20,
-    borderRadius: 5
-  }, 
-  text : {
-    color: 'white'
-  }
+
 });
 
 //AKSNDKJNASKJDBKHASBDHKJ
