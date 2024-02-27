a1500d4c58a28aa421daa31ccf385bdff32a1b43
patryck17HUB <ppoumian17@alumnos.uaq.mx>
2024-02-27 14:02:40 -0600

    Confirmar 1

--- App.jsx
+++ App.jsx
@@ -17,12 +17,20 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container : {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button : {
+    backgroundColor: 'blue',
+    padding: 20,
+    borderRadius: 5
+  }, 
+  text : {
+    color: 'white'
+  }
 });
 
 //AKSNDKJNASKJDBKHASBDHKJ
