ce49ec3ac44863f3fc0ff5583350c07e26c8ad21
patryck17HUB <ppoumian17@alumnos.uaq.mx>
2024-02-26 18:35:55 -0600

    A

--- App.jsx
+++ App.jsx
@@ -1,10 +1,20 @@
-// AAAASDASDASDASD
-import React, { useState } from "react";
-import { View, Image, Text, TouchableOpacity, StyleSheet, ScrollView } from "react-native";
-import * as ImagePicker from 'expo-image-picker';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
 
-// AAAA
-
-// SEXOSEXOSEXO
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
