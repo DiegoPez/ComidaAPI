b6551ae4d5590cc4ec1634159612ad8593f56b30
patryck17HUB <ppoumian17@alumnos.uaq.mx>
2024-02-27 14:00:41 -0600

    INTENTO3

--- App.jsx
+++ App.jsx
@@ -1,13 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-<<<<<<< HEAD
 // SKAKDKSAMKLODNALNSDLKNASLKDN
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-=======
->>>>>>> a059af2254d9768824955ce5c49cfb4b646a9050
 export default function App() {
   return (
     <View style={styles.container}>
