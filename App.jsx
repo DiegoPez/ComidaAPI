1fa2395d16822ceb1421438c1e5aaf4f29c1184e
patryck17HUB <ppoumian17@alumnos.uaq.mx>
2024-02-27 19:42:28 -0600

    no

--- App.jsx
+++ App.jsx
@@ -1,13 +1,10 @@
 import React, { useState } from "react";
 import { View, Image, Text, TouchableOpacity, StyleSheet, ScrollView } from "react-native";
-import * as ImagePicker from 'expo-image-picker';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
   return (
     <View>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
