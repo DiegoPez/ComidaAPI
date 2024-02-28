d79ac9eebe24a7daafe55018d54bf0ba15a709a3
patryck17HUB <ppoumian17@alumnos.uaq.mx>
2024-02-27 20:41:43 -0600

    Version 1.0

--- App.jsx
+++ App.jsx
@@ -1,53 +1,85 @@
+// IMPORTACIONES	
 import React, { useState } from "react";
 import { View, Image, Text, TouchableOpacity, StyleSheet, ScrollView } from "react-native";
-import * as ImagePicker from 'expo-image-picker';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
-// SKAKDKSAMKLODNALNSDLKNASLKDN
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-// SKAKDKSAMKLODNALNSDLKNASLKDN
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+// CODIGO
 export default function App() {
+
+  // DEFINIR VARIABLES
+  const [recetasData, setRecetasData] = useState(null);
+  const [error, setError] = useState(null);
+
+  API_KEY = "f0ee95f9076a471f9a7957b95742af3a"
+
+  // DEFINIR FUNCIONES
+
+  const getRecipes = async () => {
+    try {
+      const response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&query=pasta&maxFat=25&number=2`);
+      const data = await response.json();
+      console.log(data);
+      if (data.error) {
+        setError(data.error.message);
+      } else {
+        setRecetasData(data);
+      }
+    } catch (err) {
+      setError('Error finding recetas data');
+    }
+  };
+
+  // RETORNO DE LA VISTA
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+
+      <TouchableOpacity style={styles.button} onPress={getRecipes}>
+      <Text style={styles.buttonText}>Get recipes</Text>
+      </TouchableOpacity>
+
+      <ScrollView>
+      {error && <Text>{error}</Text>}
+      {recetasData && (
+
+          <View>
+            <Text>{recetasData.results[0].title}</Text>
+
+           <Image
+           style={styles.recetaImage}
+           source={{uri: recetasData.results[0].image}  }
+           />
+
+          </View>
+        )}
+      {error && <Text>{error}</Text>}
+      </ScrollView>
+
     </View>
   );
 }
 
+// ESTILOS
 const styles = StyleSheet.create({
-
-  container : {
+  container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
-  button : {
-    backgroundColor: 'blue',
+  button: {
+    backgroundColor: "blue",
     padding: 20,
     borderRadius: 5
-  }, 
-  text : {
-    color: 'white'
   },
-  image : {
-    width: 200,
-    height: 200,
-    borderRadius: 100
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 20
   },
-  gift : {
-    width: 100,
-    height: 100,
-    borderRadius: 50
+  recetaImage: {
+    width: "100%",
+    height: 200
   }
 });
-
-//AKSNDKJNASKJDBKHASBDHKJ
-
-// AAAA
